60a831df87f74398128aa1dac9462edcdb44001d
RizwanRumi <rizwan.cse33@gmail.com>
2024-09-26 14:55:16 +0200

    add Event and Delegate example

---
+++ EventAndDelegatesConsoleApp/MessageService.cs
@@ -0,0 +1,9 @@
+﻿using EventAndDelegatesConsoleApp;
+
+public class MessageService()
+{
+    public void OnVideoEncoded(object source, VideoEventArgs args)
+    {
+        Console.WriteLine("MessageService: Sending a message...." + args.Video.Title);
+    }
+}
---
+++ EventAndDelegatesConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using EventAndDelegatesConsoleApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var video = new Video() { Title = "Video 1" };
+        var videoEncoder = new VideoEncoder(); // publisher
+        var mailService = new MailService(); //   subscriber
+        var messageService = new MessageService(); // subscriber
+
+        videoEncoder.VideoEncoded += mailService.OnVideoEncoded; 
+        videoEncoder.VideoEncoded += messageService.OnVideoEncoded; 
+
+        videoEncoder.Encode(video);
+    }
+}
