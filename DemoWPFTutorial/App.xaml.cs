abb085cb4d17420d964971959958f5d9cf19a978
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-09-29 13:50:59 +0200

    add demo wpf tutorial

---
+++ DemoWPFTutorial/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DemoWPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Demo WPF :)";
+            // Show the window
+            wnd.Show();            
+        }
+    }
+
+}
