73aaf5ba6581608a04c1c2698353a471d47f305d
GROUP\rizwanur.rahman <rizwanur.rahman@jenoptik.com>
2024-10-02 11:38:54 +0200

    Add databinding example using textbox, button and textblock

--- DemoWPFTutorial/App.xaml.cs
+++ DemoWPFTutorial/App.xaml.cs
@@ -11,12 +11,16 @@ namespace DemoWPFTutorial
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            // Create the startup window
-            MainWindow wnd = new MainWindow();
+            //Create the startup window
+            //MainWindow wnd = new MainWindow();
+            TestDataBindingWindow wnd = new TestDataBindingWindow();
             // Do stuff here, e.g. to the window
             wnd.Title = "Demo WPF :)";
             // Show the window
-            wnd.Show();            
+            wnd.Show();
+
+            //TestPage testPage = new TestPage();
+            //testPage.ShowsNavigationUI = true;
         }
     }
 
---
+++ DemoWPFTutorial/TestDataBindingWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoWPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for TestDataBindingWindow.xaml
+    /// </summary>
+    public partial class TestDataBindingWindow : Window, INotifyPropertyChanged
+    {
+        public TestDataBindingWindow()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        //private void btnAdd_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+        //    {
+        //        lstNames.Items.Add(txtName.Text);
+        //        txtName.Clear();
+
+        //    }
+        //}
+
+        private string boundText = string.Empty;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                OnPropertyOnChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from Code";
+        }
+
+        private void OnPropertyOnChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
