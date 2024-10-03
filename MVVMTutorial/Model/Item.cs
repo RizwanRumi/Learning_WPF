c66c0d4784f5e176d2d5d52154aafbd6654488de
GROUP\rizwanur.rahman <rizwanur.rahman@jenoptik.com>
2024-10-03 17:11:33 +0200

    add Item class in Model

---
+++ MVVMTutorial/Model/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMTutorial.Model
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
