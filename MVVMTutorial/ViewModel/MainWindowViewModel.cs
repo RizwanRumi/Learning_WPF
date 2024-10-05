6436e8483d45667201f45bc4bc276723c0dafcd9
GROUP\rizwanur.rahman <rizwanur.rahman@jenoptik.com>
2024-10-05 11:07:10 +0200

    add RelayCommand example

--- MVVMTutorial/ViewModel/MainWindowViewModel.cs
+++ MVVMTutorial/ViewModel/MainWindowViewModel.cs
@@ -7,21 +7,27 @@ namespace MVVMTutorial.ViewModel
     internal class MainWindowViewModel : ViewModelBase
     {
         public ObservableCollection<Item> Items { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => SelectedItem != null);
+        public RelayCommand SaveCommand => new RelayCommand(execute => SaveItem(), canExecute => CanSave());
+
+
         public MainWindowViewModel() 
         {
             Items = new ObservableCollection<Item>();
-            Items.Add(new Item
-            {
-                Name = "Product1",
-                SerialNumber = "0001",
-                Quantity = 5
-            });
-            Items.Add(new Item
-            {
-                Name = "Product2",
-                SerialNumber = "0002",
-                Quantity = 10
-            });
+            //Items.Add(new Item
+            //{
+            //    Name = "Product1",
+            //    SerialNumber = "0001",
+            //    Quantity = 5
+            //});
+            //Items.Add(new Item
+            //{
+            //    Name = "Product2",
+            //    SerialNumber = "0002",
+            //    Quantity = 10
+            //});
         }
 
         private Item selectedItem;
@@ -34,6 +40,32 @@ namespace MVVMTutorial.ViewModel
                 selectedItem = value;
                 OnPropertyChanged();
             }
+        }
+
+        private void AddItem()
+        {
+            Items.Add(new Item
+            {
+                Name = "NEW ITEM",
+                SerialNumber = "XXXXX",
+                Quantity = 0
+            });
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+        private void SaveItem()
+        {
+            //save to file / database
+        }
+
+        private bool CanSave()
+        {
+            // db connection permition
+            return true;
         }
     }
 }
