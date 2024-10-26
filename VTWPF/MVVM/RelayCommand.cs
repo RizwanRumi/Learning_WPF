5f80ec23d7a1286eb83577ab12764236a275a853
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-10-26 09:51:37 +0200

    add custom event raise handling example using instead of CommandManager.RequerySuggested

--- VTWPF/MVVM/RelayCommand.cs
+++ VTWPF/MVVM/RelayCommand.cs
@@ -4,22 +4,22 @@ namespace VTWPF.MVVM
 {
     public class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
 
-        public event EventHandler? CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
+        //public event EventHandler? CanExecuteChanged
+        //{
+        //    add { CommandManager.RequerySuggested += value; }
+        //    remove { CommandManager.RequerySuggested -= value; }
+        //}
 
+        public event EventHandler? CanExecuteChanged;
 
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
             this.canExecute = canExecute;
         }
-
 
         public bool CanExecute(object? parameter)
         {
@@ -29,6 +29,11 @@ namespace VTWPF.MVVM
         public void Execute(object? parameter)
         {
             execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
